13bba1ca27dbe968863d35be6fc0cff9b0c40b7b
ShreyasJagalur <112023644+ShreyasJagalur@users.noreply.github.com>
2023-10-04 13:17:07 -0700

    new trained model

--- tf2.0/models/research/object_detection/model_downloader.py
+++ tf2.0/models/research/object_detection/model_downloader.py
@@ -1,7 +1,7 @@
 import wget
-model_link = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz"
+model_link = "http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d0_coco17_tpu-32.tar.gz"
 wget.download(model_link)
 import tarfile
-tar = tarfile.open('ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gzpyth')
+tar = tarfile.open('efficientdet_d0_coco17_tpu-32.tar.gz')
 tar.extractall('.')
 tar.close()
