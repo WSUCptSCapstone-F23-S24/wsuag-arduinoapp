bac76c4e0fe689f486739bc154409770a116f8df
Shreyas Jagalur <shreyas.jagalur@wsu.edu>
2024-01-26 13:14:39 -0800

    project with yolo framework

---
+++ predict.py
@@ -0,0 +1,5 @@
+from ultralytics import YOLO
+
+model = YOLO("yolov8m-seg-custom.pt")
+
+model.predict(source="05-06-2022_13-30-56_4.png", show=True, save=True, hide_labels=False, hide_conf=False, conf=0.5, save_txt=False, save_crop=False, line_thickness=2)
