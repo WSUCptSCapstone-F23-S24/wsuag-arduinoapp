681ae34b71e2dbf62ce6fbfd5aa119f02839805d
Forest Cook <forest.cook@wsu.edu>
2024-01-27 17:14:03 -0800

    some updates:

--- predict.py
+++ predict.py
@@ -9,8 +9,14 @@ from pathlib import Path
 
 
 def slice_into_boxes(image_path,out_folder,x,y,length,buffer=0,zoom_out=0):
+    print(f"image path {image_path}")
+
+
 
     img = cv2.imread(image_path)
+
+
+
     length_base = 155
     box_size_base = 44
     height_base = 115
@@ -18,14 +24,17 @@ def slice_into_boxes(image_path,out_folder,x,y,length,buffer=0,zoom_out=0):
     first_box_offset_y = int(30*(length/length_base))
 
 
+    pre_out_path = out_folder+"/"+os.path.splitext(os.path.basename(image_path))[0] + "_pre_box_1" + os.path.splitext(image_path)[1]
+    
+
 
     crop_img = img.copy()
-
+    #cv2.imwrite(pre_out_path,crop_img)
     
 
     #crop_img = img[y:int(y+(length*(height_base/length_base))), x:int(x+length)]
     box_length =  int(length * (box_size_base / length_base))
-    cv2.rectangle(crop_img, (x, y), (int(x+length), int(y+(length*(height_base/length_base)))), (0, 0, 256), 1)
+   # cv2.rectangle(crop_img, (x, y), (int(x+length), int(y+(length*(height_base/length_base)))), (0, 0, 256), 1)
     box_1_x_1 = int(x+first_box_offset_x+buffer)
     box_1_y_1 = int(y+first_box_offset_y+buffer)
     box_1_x_2 = int((x+first_box_offset_x+box_length)-(buffer))
@@ -54,7 +63,7 @@ def slice_into_boxes(image_path,out_folder,x,y,length,buffer=0,zoom_out=0):
                       (box_2_x_2, box_2_y_2), (0, 256, 0), 1)
         cv2.rectangle(crop_img, (box_3_x_1, box_3_y_1),
                       (box_3_x_2, box_3_y_2), (256, 0, 0), 1)
-        cv2.imwrite(os.path.splitext(image_path)[0] + "_panel" + os.path.splitext(image_path)[1], crop_img)
+        #cv2.imwrite(os.path.splitext(image_path)[0] + "_panel" + os.path.splitext(image_path)[1], crop_img)
     out_path = out_folder+"/"+os.path.splitext(os.path.basename(image_path))[0] + "_box_1" + os.path.splitext(image_path)[1]
     print("out path:")
     print(out_path)
@@ -75,12 +84,11 @@ def get_input_files_list(folder):
     for file in os.listdir(folder):
         if file.endswith(".png"):
            dir_list.append( os.path.join(folder, file))
+    print(dir_list[0:8])
+    return dir_list[0:8]
 
-    print(dir_list[0:10])
-    return dir_list[0:10]
 
-
-input_images = get_input_files_list("C:\\Users\\code8\\Downloads\\6\\6") #["02-08-2022_12-00-05_1.png", "02-07-2022_10-30-04_1.png"]
+input_images = get_input_files_list("C:\\Users\\code8\\Downloads\\6 (3)\\6") #["02-08-2022_12-00-05_1.png", "02-07-2022_10-30-04_1.png"]
 
 results = model(input_images)  # return a list of Results objects
 
@@ -89,14 +97,15 @@ results = model(input_images)  # return a list of Results objects
 for i in range(0,len(input_images)):
     result = results[i]
     image = input_images[i]
+    print(f"IMAGE: {image}, {i} END")
     #img = cv2.imread(image)
 
     box_bounding_list = result.boxes.xyxy
-    print(box_bounding_list)
-    print(result.boxes.cls)
+    # print(box_bounding_list)
+    # print(result.boxes.cls)
 
     box_index = 0
-    print(result.boxes.cls)
+    #print(result.boxes.cls)
     
     found_box = True
     
@@ -117,7 +126,8 @@ for i in range(0,len(input_images)):
     # cv2.rectangle(img, corner_1, corner_2, color=(255,0,0), thickness=1)
     # cv2.imwrite("./test_results/" + image, img)   
     print("before")
-    slice_into_boxes(image,"C:\\421_project\\wsuag-arduinoapp\\test_results",corner_1[0],corner_1[1],corner_2[0]-corner_1[0],1,5)
+    slice_into_boxes(image,"C:\\421_project\\wsuag-arduinoapp\\test_results",corner_1[0],corner_1[1],corner_2[0]-corner_1[0],1,50)
     print("after")
+
 
 
