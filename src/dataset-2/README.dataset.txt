b2128765a866f9a8d8d32a5223bbcd62d135066e
sjagalur <shreyas.jagalur@wsu.edu>
2024-02-29 18:38:43 -0800

    new models

---
+++ src/dataset-2/README.dataset.txt
@@ -0,0 +1,6 @@
+# fields2 > 2024-02-28 4:33pm
+https://universe.roboflow.com/shreyas-zmpfq/fields2
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- src/predict.py
+++ src/predict.py
@@ -329,13 +329,13 @@ import cv2 as cv
 import numpy as np
 from ultralytics import YOLO
 
-model2 = YOLO('yolo-obb.pt')
-model = YOLO('yolov8m-seg-custom-4.pt')
+# model2 = YOLO('yolo-obb.pt')
+model = YOLO('yolov8m-seg-custom-7.pt')
 
 imgT = "12-05-2022_10-30-40.3.png"
 
 results = model.predict(source=imgT, conf=0.2)
-results2 = model2.predict(source=imgT, conf=0.2, save = True, hide_conf=True, hide_labels=True)
+# results2 = model2.predict(source=imgT, conf=0.2, save = True, hide_conf=True, hide_labels=True)
 
 for result in results:
     img = np.copy(result.orig_img)
@@ -374,23 +374,23 @@ for result in results:
         print(f"Isolated image saved: {output_filename}")
 
 
-for result in results2:
-    img = np.copy(result.orig_img)
-    img_name = Path(result.path).stem  
+# for result in results2:
+#     img = np.copy(result.orig_img)
+#     img_name = Path(result.path).stem  
 
-    for contour_idx, contour in enumerate(result):
+#     for contour_idx, contour in enumerate(result):
 
-        # label = result.names[result.boxes.cls.tolist().pop()]
-        print(contour.obb)
-        label = ""
-        for box in contour.obb:
-            class_id = int(box.data[0][-1])
-            label = model.names[class_id]
+#         # label = result.names[result.boxes.cls.tolist().pop()]
+#         print(contour.obb)
+#         label = ""
+#         for box in contour.obb:
+#             class_id = int(box.data[0][-1])
+#             label = model.names[class_id]
 
         
-        x1, y1, x2, y2 = contour.obb.xyxy.cpu().numpy().squeeze().astype(np.int32)
-        isolated_crop = img[y1:y2, x1:x2]
+#         x1, y1, x2, y2 = contour.obb.xyxy.cpu().numpy().squeeze().astype(np.int32)
+#         isolated_crop = img[y1:y2, x1:x2]
 
-        output_filename = f"isolated_{img_name}_{contour_idx}_obb.png"
-        cv.imwrite(output_filename, isolated_crop)
-        print(f"Isolated image saved: {output_filename}")
+#         output_filename = f"isolated_{img_name}_{contour_idx}_obb.png"
+#         cv.imwrite(output_filename, isolated_crop)
+#         print(f"Isolated image saved: {output_filename}")
